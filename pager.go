0c5b1b23d43e32f17202e9c3c9086b748bf431ad
hiscaler <hiscaler@gmail.com>
2025-07-30 10:10:03 +0800

    ...

--- pager.go
+++ pager.go
@@ -1,11 +1,11 @@
 package rpclient
 
-// Pager 带翻页的数据
+// Pager 带翻页的数据列表
 type Pager struct {
-	Page       int  `json:"page"`
-	PageSize   int  `json:"page_size"`
-	TotalCount int  `json:"total_count"`
-	PageCount  int  `json:"page_count"`
-	IsLastPage bool `json:"is_last_page"`
-	Items      any  `json:"items"`
+	Page       int  `json:"page"`         // 当前页码
+	PageSize   int  `json:"page_size"`    // 每页数据条数
+	TotalCount int  `json:"total_count"`  // 数据总数
+	PageCount  int  `json:"page_count"`   // 总页数
+	IsLastPage bool `json:"is_last_page"` // 是否最后一页
+	Items      any  `json:"items"`        // 数据
 }
