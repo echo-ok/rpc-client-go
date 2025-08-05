97d655df8e552294f777093136fdbcb4b9e50b65
hiscaler <hiscaler@gmail.com>
2025-08-05 09:47:17 +0800

    ...

--- option.go
+++ option.go
@@ -8,9 +8,9 @@ const (
 // Option NetWork Known networks are "tcp", "tcp4" (IPv4-only), "tcp6" (IPv6-only), "udp", "udp4" (IPv4-only), "udp6" (IPv6-only), "ip", "ip4" (IPv4-only), "ip6" (IPv6-only), "unix", "unixgram" and "unixpacket".
 // Codec supported codecs are "goridge" and "json"
 type Option struct {
-	Network        string   `json:"network" yaml:"network" toml:"network"`
-	Codec          string   `json:"codec" yaml:"codec" toml:"codec"`
-	LogLevel       string   `json:"log_level" yaml:"log_level" toml:"log_level"`                   // debug, info, warn, error
+	Network        string   `json:"network" yaml:"network" toml:"network"`                         // Current only support `tcp`
+	Codec          string   `json:"codec" yaml:"codec" toml:"codec"`                               // Codes: json, goridge
+	LogLevel       string   `json:"log_level" yaml:"log_level" toml:"log_level"`                   // Level: debug, info, warn, error
 	SensitiveWords []string `json:"sensitive_words" yaml:"sensitive_words" toml:"sensitive_words"` // Sensitive words
 }
 
